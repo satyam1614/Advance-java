70283b8f786e1563f369b300d3629749147416f6
satyam1614 <satyamyadav162000@.com>
2026-02-19 16:51:25 +0530

    done

--- jdbc/src/com/rays/preparedstatement/TestMarksheetModel.java
+++ jdbc/src/com/rays/preparedstatement/TestMarksheetModel.java
@@ -1,16 +1,18 @@
 package com.rays.preparedstatement;
 
-
+import java.util.Iterator;
+import java.util.List;
 
 public class TestMarksheetModel {
 
 	public static void main(String[] args) throws Exception {
 
-		//testAdd();
+//		testAdd();
 		//testUpdate();
 		// testDelete();
-		  testfindByRollNo(); 
-		   
+		 // testfindByRollNo(); 
+//		   testfindByPk();
+		Testsearch();
 	}
 
 	public static void testAdd() throws Exception {
@@ -18,7 +20,7 @@ public class TestMarksheetModel {
 		MarksheetModel model = new MarksheetModel();
 		MarksheetBean bean = new MarksheetBean();
 		
-		bean.setId(15);
+		bean.setId(16);
 		bean.setRollNo(115);
 		bean.setName("punnit");
 		bean.setPhy(50);
@@ -61,7 +63,6 @@ public class TestMarksheetModel {
 		 MarksheetModel model = new MarksheetModel();
 		 MarksheetBean bean = new MarksheetBean();
 		 
-		    bean = model.findByRollNo(115);
 		   System.out.println(bean.getId());
 		   System.out.println(bean.getRollNo());
 		   System.out.println(bean.getName());
@@ -70,5 +71,44 @@ public class TestMarksheetModel {
 		   System.out.println(bean.getMaths());
 		   System.out.println(bean.getDept_id());
 		   
+		   bean = model.findByRollNo(115);
 	 }
-}
+	 public static void  testfindByPk() throws Exception {
+		  
+		 MarksheetModel model = new MarksheetModel ();
+		 MarksheetBean bean = new MarksheetBean();
+		 
+		    System.out.println(bean.getId());
+		    System.out.println(bean.getRollNo());
+		    System.out.println(bean.getName());
+			   System.out.println(bean.getPhy());
+			   System.out.println(bean.getHindi());
+			   System.out.println(bean.getMaths());
+			   System.out.println(bean.getDept_id());
+		 
+			   bean = model.findBypk(10);
+	 }
+	 public static void Testsearch() throws Exception {
+
+		        MarksheetBean bean = new MarksheetBean();
+		        MarksheetModel model = new MarksheetModel();
+		        
+		        bean.setName("p");
+		        
+		        List list = model.search(bean);
+		        
+		        Iterator<MarksheetBean> it = list.iterator();
+		        while(it.hasNext()) {
+		        	bean = it.next();
+		            System.out.println("ID: " + bean.getId());
+		            System.out.println("Roll No: " + bean.getRollNo());
+		            System.out.println("Name: " + bean.getName());
+		            System.out.println("Physics: " + bean.getPhy());
+		            System.out.println("Hindi: " + bean.getHindi());
+		            System.out.println("Maths: " + bean.getMaths());
+		            System.out.println("Dept_id: " + bean.getDept_id());
+		          
+		        }
+		    }
+		}
+
